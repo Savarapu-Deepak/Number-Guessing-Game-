7fb3e48897a506481b936abd350a49a5f603ffa5
Deepak <savarapudeepak9@gmail.com>
2023-06-26 15:03:02 +0530

    Number Guessing Game

---
+++ main.py
@@ -0,0 +1,23 @@
+# Number Guessing Game.
+import random
+
+lower_Bound = int(input('Enter Lower Bound Value : '))
+upper_Bound = int(input('Enter Upper Bound Value : '))
+
+number = random.randint(lower_Bound, upper_Bound)
+print('You Have Only 7 Guessing Chances.')
+
+
+check = 1
+while check <= 7:
+    guess = int(input('Guess Any Number With in 1 - 100 : '))
+    if guess > number:
+        print('Your Guess is High......')
+    elif guess == number:
+        print('Congrats.....You did it in {} turn'.format(check))
+        break
+    else:
+        print('Your Guess is Low')
+    check += 1
+
+
